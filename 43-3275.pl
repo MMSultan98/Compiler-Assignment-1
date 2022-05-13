f726928f48f3318bfa7ef935d1f4db2c48dd9bed
Mohammad Marwan Sultan <mohammad.marwan.sultan@gmail.com>
2022-05-13 18:26:44 +0200

    Modified conjunction between clauses

--- 43-3275.pl
+++ 43-3275.pl
@@ -1,23 +1,37 @@
-% Start variable
+% 1- Start variable
 s(DSS, A, Z) :- declarative_sentences(DSS, A, Z).
 s(ISS, A, Z) :- interrogatory_sentences(ISS, A, Z).
 
 
-% Structures
+% 2- Structures
+
+% 2.1 - Sentences
+% 2.1.1 - Declaritive sentences
 declarative_sentences(dec_ss(DS), A, Z) :- declarative_sentence(DS, A, Z).
-declarative_sentences(dec_ss(DS,CON,DSS), A, Z) :- declarative_sentence(DS, A, B), conjunction(CON, B, C), declarative_sentences(DSS, C, Z).
+declarative_sentences(dec_ss(DSC,CON,DS), A, Z) :- declarative_sentence_comma(DSC, A, B), conjunction(CON, B, C), declarative_sentence(DS, C, Z).
+
+declarative_sentence_comma(dec_sc(DS), A, Z) :- declarative_sentence(DS, A, Z).
+declarative_sentence_comma(dec_sc(DS,DSC), A, Z) :- declarative_sentence(DS, A, B), declarative_sentence_comma(DSC, B, Z).
 
 declarative_sentence(dec_s(NPS,VPS), A, Z) :- noun_phrases(NPS, A, B), verb_phrases_past(VPS, B, Z).
 declarative_sentence(dec_s(NPS,VPS), A, Z) :- noun_phrases(NPS, A, B), verb_phrases_past_2(VPS, B, Z).
 
+% 2.1.2 - Interrogatory sentences
 interrogatory_sentences(int_ss(IS), A, Z) :- interrogatory_sentence(IS, A, Z).
-interrogatory_sentences(int_ss(IS,CON,ISS), A, Z) :- interrogatory_sentence(IS, A, B), conjunction(CON, B, C), interrogatory_sentences(ISS, C, Z).
+interrogatory_sentences(int_ss(ISC,CON,IS), A, Z) :- interrogatory_sentence_comma(ISC, A, B), conjunction(CON, B, C), interrogatory_sentence(IS, C, Z).
+
+interrogatory_sentence_comma(int_sc(IS), A, Z) :- interrogatory_sentence(IS, A, Z).
+interrogatory_sentence_comma(int_sc(IS,ISC), A, Z) :- interrogatory_sentence(IS, A, B), interrogatory_sentence_comma(ISC, B, Z).
 
 interrogatory_sentence(int_s(IP,VPS), A, Z) :- interrogative_pronoun(IP, A, B), verb_phrases_past(VPS, B, Z).
 interrogatory_sentence(int_s(IP,VPS1,VPS2), A, Z) :- interrogative_pronoun(IP, A, B), verb_phrases_past_aux(VPS1, B, C), verb_phrases_inf(VPS2, C, Z).
 
+% 2.2- Noun phrases
 noun_phrases(nps(NP), A, Z) :- noun_phrase(NP, A, Z).
-noun_phrases(nps(NP,CON,NPS), A, Z) :- noun_phrase(NP, A, B), conjunction(CON, B, C), noun_phrases(NPS, C, Z).
+noun_phrases(nps(NPC,CON,NP), A, Z) :- noun_phrase_comma(NPC, A, B), conjunction(CON, B, C), noun_phrase(NP, C, Z).
+
+noun_phrase_comma(npc(NP), A, Z) :- noun_phrase(NP, A, Z).
+noun_phrase_comma(npc(NP,NPC), A, Z) :- noun_phrase(NP, A, B), noun_phrase_comma(NPC, B, Z).
 
 noun_phrase(np(N), A, Z) :- noun(N, A, Z).
 noun_phrase(np(N,PP), A, Z) :- noun(N, A, B), perpositional_phrase(PP, B, Z).
@@ -36,65 +50,130 @@ noun_phrase(np(DET,N,PP,OPP), A, Z) :- determiner(DET, A, B), noun(N, B, C), per
 noun_phrase(np(DET,AJP,N,OPP), A, Z) :- determiner(DET, A, B), adjective_phrase(AJP, B, C), noun(N, C, D), object_pronoun_phrase(OPP, D, Z).
 noun_phrase(np(DET,AJP,N,PP,OPP), A, Z) :- determiner(DET, A, B), adjective_phrase(AJP, B, C), noun(N, C, D), perpositional_phrase(PP, D, E), object_pronoun_phrase(OPP, E, Z).
 
+% 2.3- Verb phrases
+% 2.3.1- Verb phrases (past tense verbs)
 verb_phrases_past(vps(VP), A, Z) :- verb_phrase_past(VP, A, Z).
-verb_phrases_past(vps(VP,CON,VPS), A, Z) :- verb_phrase_past(VP, A, B), conjunction(CON, B, C), verb_phrases_past(VPS, C, Z).
+verb_phrases_past(vps(VPC,CON,VP), A, Z) :- verb_phrase_past_comma(VPC, A, B), conjunction(CON, B, C), verb_phrase_past(VP, C, Z).
 
-verb_phrases_past_2(vps(VP), A, Z) :- verb_phrase_past_2(VP, A, Z).
-verb_phrases_past_2(vps(VP,CON,VPS), A, Z) :- verb_phrase_past_2(VP, A, B), conjunction(CON, B, C), verb_phrases_past_2(VPS, C, Z).
-
-verb_phrases_past_aux(vps(VP), A, Z) :- verb_phrase_past_aux(VP, A, Z).
-verb_phrases_past_aux(vps(VP,CON,VPS), A, Z) :- verb_phrase_past_aux(VP, A, B), conjunction(CON, B, C), verb_phrases_past_aux(VPS, C, Z).
-
-verb_phrases_inf(vps(VP), A, Z) :- verb_phrase_inf(VP, A, Z).
-verb_phrases_inf(vps(VP,CON,VPS), A, Z) :- verb_phrase_inf(VP, A, B), conjunction(CON, B, C), verb_phrases_inf(VPS, C, Z).
+verb_phrase_past_comma(vpc(VP), A, Z) :- verb_phrase_past(VP, A, Z).
+verb_phrase_past_comma(vpc(VP,VPC), A, Z) :- verb_phrase_past(VP, A, B), verb_phrase_past_comma(VPC, B, Z).
 
 verb_phrase_past(vp(VA), A, Z) :- verb_past_adverb(VA, A, Z).
-verb_phrase_past(vp(VA,NPS), A, Z) :- verb_past_adverb(VA, A, B), noun_phrases(NPS, B, Z).
-verb_phrase_past(vp(VA,PP), A, Z) :- verb_past_adverb(VA, A, B), perpositional_phrase(PP, B, Z).
-verb_phrase_past(vp(VA,NPS,PP), A, Z) :- verb_past_adverb(VA, A, B), noun_phrases(NPS, B, C), perpositional_phrase(PP, C, Z).
-verb_phrase_past(vp(V,NPS,AVP), A, Z) :- verb_past(V, A, B), noun_phrases(NPS, B, C), adverb_phrase(AVP, C, Z).
-verb_phrase_past(vp(V,NPS,PP,AVP), A, Z) :- verb_past(V, A, B), noun_phrases(NPS, B, C), perpositional_phrase(PP, C, D), adverb_phrase(AVP, D, Z).
+verb_phrase_past(vp(VAS,NPS), A, Z) :- verb_past_adverbs(VAS, A, B), noun_phrases(NPS, B, Z).
+verb_phrase_past(vp(VAS,PP), A, Z) :- verb_past_adverbs(VAS, A, B), perpositional_phrase(PP, B, Z).
+verb_phrase_past(vp(VAS,NPS,PP), A, Z) :- verb_past_adverbs(VAS, A, B), noun_phrases(NPS, B, C), perpositional_phrase(PP, C, Z).
+verb_phrase_past(vp(VS,NPS,AVP), A, Z) :- verbs_past(VS, A, B), noun_phrases(NPS, B, C), adverb_phrase(AVP, C, Z).
+verb_phrase_past(vp(VS,NPS,PP,AVP), A, Z) :- verbs_past(VS, A, B), noun_phrases(NPS, B, C), perpositional_phrase(PP, C, D), adverb_phrase(AVP, D, Z).
+
+% 2.3.2- Verb phrases (past tense verbs with 2 objects)
+verb_phrases_past_2(vps(VP), A, Z) :- verb_phrase_past_2(VP, A, Z).
+verb_phrases_past_2(vps(VPC,CON,VP), A, Z) :- verb_phrase_past_2_comma(VPC, A, B), conjunction(CON, B, C), verb_phrase_past_2(VP, C, Z).
+
+verb_phrase_past_2_comma(vpc(VP), A, Z) :- verb_phrase_past_2(VP, A, Z).
+verb_phrase_past_2_comma(vpc(VP,VPC), A, Z) :- verb_phrase_past_2(VP, A, B), verb_phrase_past_2_comma(VPC, B, Z).
 
 verb_phrase_past_2(vp(VA), A, Z) :- verb_past_2_adverb(VA, A, Z).
-verb_phrase_past_2(vp(VA,NPS1,NPS2), A, Z) :- verb_past_2_adverb(VA, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, Z).
-verb_phrase_past_2(vp(VA,NPS1,NPS2,PP), A, Z) :- verb_past_2_adverb(VA, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, D), perpositional_phrase(PP, D, Z).
-verb_phrase_past_2(vp(V,NPS1,NPS2,AVP), A, Z) :- verb_past_2(V, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, D), adverb_phrase(AVP, D, Z).
-verb_phrase_past_2(vp(V,NPS1,NPS2,PP,AVP), A, Z) :- verb_past_2(V, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, D), perpositional_phrase(PP, D, E), adverb_phrase(AVP, E, Z).
+verb_phrase_past_2(vp(VAS,NPS1,NPS2), A, Z) :- verb_past_2_adverbs(VAS, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, Z).
+verb_phrase_past_2(vp(VAS,NPS1,NPS2,PP), A, Z) :- verb_past_2_adverbs(VAS, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, D), perpositional_phrase(PP, D, Z).
+verb_phrase_past_2(vp(VS,NPS1,NPS2,AVP), A, Z) :- verbs_past_2(VS, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, D), adverb_phrase(AVP, D, Z).
+verb_phrase_past_2(vp(VS,NPS1,NPS2,PP,AVP), A, Z) :- verbs_past_2(VS, A, B), noun_phrases(NPS1, B, C), noun_phrases(NPS2, C, D), perpositional_phrase(PP, D, E), adverb_phrase(AVP, E, Z).
+
+% 2.3.3- Verb phrases (past tense auxiliary verbs)
+verb_phrases_past_aux(vps(VP), A, Z) :- verb_phrase_past_aux(VP, A, Z).
+verb_phrases_past_aux(vps(VPC,CON,VP), A, Z) :- verb_phrase_past_aux_comma(VPC, A, B), conjunction(CON, B, C), verb_phrase_past_aux(VP, C, Z).
+
+verb_phrase_past_aux_comma(vpc(VP), A, Z) :- verb_phrase_past_aux(VP, A, Z).
+verb_phrase_past_aux_comma(vpc(VP,VPC), A, Z) :- verb_phrase_past_aux(VP, A, B), verb_phrase_past_aux_comma(VPC, B, Z).
 
 verb_phrase_past_aux(vp(V,NPS), A, Z) :- verb_past_aux(V, A, B), noun_phrases(NPS, B, Z).
 
+% 2.3.4- Verb phrases (infinitive verbs)
+verb_phrases_inf(vps(VP), A, Z) :- verb_phrase_inf(VP, A, Z).
+verb_phrases_inf(vps(VPC,CON,VP), A, Z) :- verb_phrase_inf_comma(VPC, A, B), conjunction(CON, B, C), verb_phrase_inf(VP, C, Z).
+
+verb_phrase_inf_comma(vpc(VP), A, Z) :- verb_phrase_inf(VP, A, Z).
+verb_phrase_inf_comma(vpc(VP,VPC), A, Z) :- verb_phrase_inf(VP, A, B), verb_phrase_inf_comma(VPC, B, Z).
+
 verb_phrase_inf(vp(VA), A, Z) :- verb_inf_adverb(VA, A, Z).
-verb_phrase_inf(vp(VA,PP), A, Z) :- verb_inf_adverb(VA, A, B), perpositional_phrase(PP, B, Z).
+verb_phrase_inf(vp(VAS,PP), A, Z) :- verb_inf_adverbs(VAS, A, B), perpositional_phrase(PP, B, Z).
+
+% 2.4- Verbs with adverb phrases
+% 2.4.1- Verbs with adverb phrases (past tense verbs)
+verb_past_adverbs(vas(VA), A, Z) :- verb_past_adverb(VA, A, Z).
+verb_past_adverbs(vas(VAC,CON,VA), A, Z) :- verb_past_adverb_comma(VAC, A, B), conjunction(CON, B, C), verb_past_adverb(VA, C, Z).
+
+verb_past_adverb_comma(vac(VA), A, Z) :- verb_past_adverb(VA, A, Z).
+verb_past_adverb_comma(vac(VA,VAC), A, Z) :- verb_past_adverb(VA, A, B), verb_past_adverb_comma(VAC, B, Z).
 
 verb_past_adverb(va(V), A, Z) :- verb_past(V, A, Z).
 verb_past_adverb(va(AVP,V), A, Z) :- adverb_phrase(AVP, A, B), verb_past(V, B, Z).
 verb_past_adverb(va(V,AVP), A, Z) :- verb_past(V, A, B), adverb_phrase(AVP, B, Z).
 
+% 2.4.2- Verbs with adverb phrases (past tense verbs with 2 objects)
+verb_past_2_adverbs(vas(VA), A, Z) :- verb_past_2_adverb(VA, A, Z).
+verb_past_2_adverbs(vas(VAC,CON,VA), A, Z) :- verb_past_2_adverb_comma(VAC, A, B), conjunction(CON, B, C), verb_past_2_adverb(VA, C, Z).
+
+verb_past_2_adverb_comma(vac(VA), A, Z) :- verb_past_2_adverb(VA, A, Z).
+verb_past_2_adverb_comma(vac(VA,VAC), A, Z) :- verb_past_2_adverb(VA, A, B), verb_past_2_adverb_comma(VAC, B, Z).
+
 verb_past_2_adverb(va(V), A, Z) :- verb_past_2(V, A, Z).
 verb_past_2_adverb(va(AVP,V), A, Z) :- adverb_phrase(AVP, A, B), verb_past_2(V, B, Z).
 verb_past_2_adverb(va(V,AVP), A, Z) :- verb_past_2(V, A, B), adverb_phrase(AVP, B, Z).
+
+% 2.4.3- Verbs with adverb phrases (infinitive verbs)
+verb_inf_adverbs(vas(VA), A, Z) :- verb_inf_adverb(VA, A, Z).
+verb_inf_adverbs(vas(VAC,CON,VA), A, Z) :- verb_inf_adverb_comma(VAC, A, B), conjunction(CON, B, C), verb_inf_adverb(VA, C, Z).
+
+verb_inf_adverb_comma(vac(VA), A, Z) :- verb_inf_adverb(VA, A, Z).
+verb_inf_adverb_comma(vac(VA,VAC), A, Z) :- verb_inf_adverb(VA, A, B), verb_inf_adverb_comma(VAC, B, Z).
 
 verb_inf_adverb(va(V), A, Z) :- verb_inf(V, A, Z).
 verb_inf_adverb(va(AVP,V), A, Z) :- adverb_phrase(AVP, A, B), verb_inf(V, B, Z).
 verb_inf_adverb(va(V,AVP), A, Z) :- verb_inf(V, A, B), adverb_phrase(AVP, B, Z).
 
+% 2.5- Verbs
+% 2.5.1- Verbs (past tense)
+verbs_past(vs(V), A, Z) :- verb_past(V, A, Z).
+verbs_past(vs(VC,CON,V), A, Z) :- verb_past_comma(VC, A, B), conjunction(CON, B, C), verb_past(V, C, Z).
+
+verb_past_comma(vc(V), A, Z) :- verb_past(V, A, Z).
+verb_past_comma(vc(V,VC), A, Z) :- verb_past(V, A, B), verb_past_comma(VC, B, Z).
+
+% 2.5.2- Verbs (past tense with 2 objects)
+verbs_past_2(vs(V), A, Z) :- verb_past_2(V, A, Z).
+verbs_past_2(vs(VC,CON,V), A, Z) :- verb_past_2_comma(VC, A, B), conjunction(CON, B, C), verb_past_2(V, C, Z).
+
+verb_past_2_comma(vc(V), A, Z) :- verb_past_2(V, A, Z).
+verb_past_2_comma(vc(V,VC), A, Z) :- verb_past_2(V, A, B), verb_past_2_comma(VC, B, Z).
+
+% 2.5.3- Verbs (infinitive)
+verbs_inf(vs(V), A, Z) :- verb_inf(V, A, Z).
+verbs_inf(vs(VC,CON,V), A, Z) :- verb_inf_comma(VC, A, B), conjunction(CON, B, C), verb_inf(V, C, Z).
+
+verb_inf_comma(vc(V), A, Z) :- verb_inf(V, A, Z).
+verb_inf_comma(vc(V,VC), A, Z) :- verb_inf(V, A, B), verb_inf_comma(VC, B, Z).
+
+% 2.6- Adverb phrases
 adverb_phrase(avp(AV), A, Z) :- adverb(AV, A, Z).
 adverb_phrase(avp(AVC,CON,AV), A, Z) :- adverb_comma(AVC, A, B), conjunction(CON, B, C), adverb(AV, C, Z).
 
 adverb_comma(avc(AV), A, Z) :- adverb(AV, A, Z).
 adverb_comma(avc(AV,AVC), A, Z) :- adverb(AV, A, B), adverb_comma(AVC, B, Z).
 
+% 2.7- Adjective phrases
 adjective_phrase(ajp(AJ), A, Z) :- adjective(AJ, A, Z).
 adjective_phrase(ajp(AJ,AJP), A, Z) :- adjective(AJ, A, B), adjective_phrase(AJP, B, Z).
 
+% 2.8- Perpositional phrases
 perpositional_phrase(pp(P,NP), A, Z) :- preposition(P, A, B), noun_phrase(NP, B, Z).
 perpositional_phrase(pp(P,NP,PP), A, Z) :- preposition(P, A, B), noun_phrase(NP, B, C), perpositional_phrase(PP, C, Z).
 
+% 2.9- Object pronoun phrases
 object_pronoun_phrase(opp(OP,PP,V), A, Z) :- object_pronoun(OP, A, B), personal_pronoun(PP, B, C), verb_past(V, C, Z).
 object_pronoun_phrase(opp(OP,NP,V), A, Z) :- object_pronoun(OP, A, B), noun_phrase(NP, B, C), verb_past(V, C, Z).
 
 
-% Categories
+% 3- Categories
 determiner(d(DET), [DET|X], X) :- d(DET).
 noun(n(N), [N|X], X) :- n(N).
 verb_past(v(V), [V|X], X) :- v_past(V).
@@ -110,7 +189,9 @@ object_pronoun(op(OP), [OP|X], X) :- opn(OP).
 conjunction(con(CON), [CON|X], X) :- con(CON).
 
 
-% Lexicon
+% 4- Lexicon
+
+% 4.1- Determiners
 d(the).
 d(a).
 d(an).
@@ -119,6 +200,7 @@ d(all).
 d(some).
 d(many).
 
+% 4.2- Nouns
 n(cat).
 n(bat).
 n(boy).
@@ -132,10 +214,15 @@ n(envelope).
 n(shed).
 n(building).
 n(tree).
+n(student).
 n(students).
+n(professor).
 n(professors).
+n(lecturer).
 n(lecturers).
+n(scientist).
 n(scientists).
+n(researcher).
 n(researchers).
 n(flower).
 n(telescope).
@@ -147,6 +234,7 @@ n(money).
 n(meal).
 n(food).
 
+% 4.3- Verbs (past tense)
 v_past(saw).
 v_past(ate).
 v_past(drank).
@@ -179,6 +267,7 @@ v_past(knew).
 v_past(thought).
 v_past(made).
 
+% 4.4- Verbs (past tense with 2 objects)
 v_past_2(gave).
 v_past_2(showed).
 v_past_2(taught).
@@ -190,8 +279,10 @@ v_past_2(got).
 v_past_2(made).
 v_past_2(cooked).
 
+% 4.5- Verbs (past tense auxiliary)
 v_past_aux(did).
 
+% 4.6- Verbs (infinitive)
 v_inf(do).
 v_inf(eat).
 v_inf(drink).
@@ -203,6 +294,7 @@ v_inf(make).
 v_inf(get).
 v_inf(cook).
 
+% 4.7- Adjectives
 adj(young).
 adj(old).
 adj(large).
@@ -230,6 +322,7 @@ adj(clean).
 adj(clear).
 adj(clever).
 
+% 4.8- Adverbs
 adv(quickly).
 adv(slowly).
 adv(boldly).
@@ -258,6 +351,7 @@ adv(rapidly).
 adv(daily).
 adv(secretly).
 
+% 4.9- Prepositions
 p(in).
 p(on).
 p(at).
@@ -275,6 +369,7 @@ p(up).
 p(down).
 p(over).
 
+% 4.10- Personal pronouns
 pp(i).
 pp(we).
 pp(you).
@@ -283,9 +378,12 @@ pp(he).
 pp(she).
 pp(it).
 
+% 4.11- Interrogative pronouns
 ip(who).
 ip(what).
 
+% 4.12- Object pronouns
 opn(whom).
 
+% 4.13- Conjunctions
 con(and).
